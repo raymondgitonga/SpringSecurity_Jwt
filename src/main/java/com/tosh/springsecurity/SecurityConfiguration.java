53bc5751f3d43445d965ff8a470b793aa74df47b
raymond gitonga <raytosh95@gmail.com>
2020-04-21 05:55:55 +0300

    Update security config

--- src/main/java/com/tosh/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/tosh/springsecurity/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity security) throws Exception {
          security.csrf().disable()
-                .authorizeRequests().antMatchers(".authenticate").permitAll().anyRequest().authenticated();
+                .authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
     }
 
     @Override
